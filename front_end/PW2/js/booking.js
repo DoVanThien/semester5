b73a9f8eb7aaffd40821093114e38694c617e967
DoVanThien <vicetone125@gmail.com>
2021-11-18 15:31:11 +0300

    update PW2 + PW3

--- front_end/PW2/js/booking.js
+++ front_end/PW2/js/booking.js
@@ -3,9 +3,10 @@ const gridHotel = document.getElementById('grid-hotel');
 
 searchBtn.addEventListener('click', getHotels);
 
+
 function getHotels(){
     let inputTxt = document.getElementById('destination').value.trim();
-    fetch(`https://api.unsplash.com/search/photos?client_id=wt3siU2g9HdgFOKnCWn5WBIBWjkZC6e5BHu6zY78zbM&per_page=20&query=moscow`)
+    fetch(`https://api.unsplash.com/search/photos?client_id=wt3siU2g9HdgFOKnCWn5WBIBWjkZC6e5BHu6zY78zbM&per_page=20&query=${inputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
@@ -15,7 +16,7 @@ function getHotels(){
                 <div class="grid-item" id = ${hotel.id}>
                     <a href="#" class="hotel-link">
                         <div class="background-hotel-img">
-                            <img src="${hotel.urls.thumb}" alt="${hotel.alt_description}" class="hotel-img">
+                            <img src="${hotel.urls.regular}" alt="${hotel.alt_description}" class="hotel-img">
                         </div>
                         <h5 class="hotel-name">${hotel.description}</h5>
                         <p class="hotel-price">${hotel.likes}$/night</p>
